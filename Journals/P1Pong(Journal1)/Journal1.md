8034b3445243a1870bb96bae67d30d85d7356429
Caterina Martin <caterinamartin6@gmail.com>
2026-09-22 15:31:57 -0400

    journal and game updates

--- Journals/P1Pong(Journal1)/Journal1.md
+++ Journals/P1Pong(Journal1)/Journal1.md
@@ -5,8 +5,16 @@ Goal: to change the background color whenever a paddle hits the ball
 - Created a new script for the Background asset. 
 - Added a camera and color variable. 
 - Added a OnCollisionEnter2D constructor which changes the color of the background if the game object (ball) enters/collides with another object of a certain tag, in this case being "Paddle".
-- Had to figure out how to utilize tags within game objects.
+- Figured out how to utilize tags within game objects via research.
 - Created a tag named "Paddle" and attached it to each of the paddles (PaddlePlayer and PaddleCPU).
-- Realized that I actually needed to put all of this code into the Ball object instead of the Background object so I changed that. 
+- Realized that I actually needed to put all of this code into the Ball object script instead of the Background object script so I changed that. 
+
+
+
+==2026/09/22==
+
+Goals: 
+1. To keep the velocity constant on the ball game object even if there is collision with another game object (paddle or court).
+2. To make two balls spawn from every collision of a ball with a paddle. 
 
 
--- P1_Pong/P1_Pong (Assigment1)/Assets/Scripts/Ball.cs
+++ P1_Pong/P1_Pong (Assigment1)/Assets/Scripts/Ball.cs
@@ -1,4 +1,5 @@
 using System;
+using Unity.VisualScripting;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
@@ -6,9 +7,12 @@ public class Ball : MonoBehaviour
 {
     private Rigidbody2D _rigidBody;
 
-    public float speed = 100.0f;
+    public float speed = 12.0f;
     public Camera mainCamera;
     public Color collisionColor;
+
+    [Header("MultiBall Settings")]
+    public GameObject ballPrefab;
 
     private void Awake()
     {
@@ -22,11 +26,21 @@ public class Ball : MonoBehaviour
             mainCamera.clearFlags = CameraClearFlags.SolidColor;
 
         collisionColor = Random.ColorHSV();
+
+
     }
 
     private void Update()
     {
         collisionColor = Random.ColorHSV();
+    }
+
+    private void FixedUpdate()
+    {
+        if (_rigidBody.linearVelocity.sqrMagnitude > 0.01f)
+        {
+            _rigidBody.linearVelocity = _rigidBody.linearVelocity.normalized * speed;
+        }
     }
 
     public void ResetBall()
@@ -43,7 +57,22 @@ public class Ball : MonoBehaviour
 
         Vector2 direction = new Vector2(x, y);
 
-        _rigidBody.AddForce(direction * speed);
+        _rigidBody.linearVelocity = direction * speed;
+
+        GameObject newBallObject = Instantiate(ballPrefab, Vector3.zero, Quaternion.identity);
+
+        Ball newBallScript = newBallObject.GetComponent<Ball>();
+
+        if (newBallScript != null)
+        {
+            newBallScript.AddStartingForce();
+        }
+    
+        else
+        {
+            Debug.LogWarning("Please drag your Ball Prefab into the 'Ball Prefab' slot in the Inspector!");
+        }
+
     }
 
 
@@ -52,6 +81,25 @@ public class Ball : MonoBehaviour
         if (collision.gameObject.CompareTag("Paddle"))
             mainCamera.backgroundColor = collisionColor;
     }
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        GameManager gameManager = FindFirstObjectByType<GameManager>();
+
+        if (gameManager != null)
+        {
+            if (other.CompareTag("CourtLeft"))
+            {
+                gameManager.CourtTriggered(0);
+            }
+            else if (other.CompareTag("CourtRight"))
+            {
+                gameManager.CourtTriggered(1);
+            }
+        }
+    }
+
 }
 
 
